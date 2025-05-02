de37a4e501a7b223c23dc42aa353865157e8c9b8
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-05-02 15:11:52 -0500

    website done

---
+++ home/script.js
@@ -0,0 +1,19 @@
+document.querySelector(".home-button").addEventListener("click", () => {
+    window.location.href = "/home/home.html";
+});
+
+document.querySelector(".bio-button").addEventListener("click", () => {
+    window.location.href = "/bio/bio.html";
+});
+
+document.querySelector(".experience-button").addEventListener("click", () => {
+    window.location.href = "/experience/experience.html";
+});
+
+document.querySelector(".portfolio-button").addEventListener("click", () => {
+    window.location.href = "/portfolio/portfolio.html";
+});
+
+document.querySelector(".resume-button").addEventListener("click", () => {
+    window.location.href = "/home/BPatelResume.pdf";
+});
